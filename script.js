79fdb94c3bc26f8ba99234e5f99a6eb98e71bebb
Diya Shah <diya68230@gmail.com>
2026-04-18 21:56:02 +0530

    Completed base UI layout with HTML and CSS

---
+++ script.js
@@ -0,0 +1,35 @@
+let play =document.getElementById('play');
+let progressbar =document.getElementById('progressbar');
+let audio = new Audio('Audio/2.mp3');
+play.addEventListener('click',() => {
+    if(audio.paused || audio.currentTime == 0){
+        audio.play();
+        play.classList.remove('fa-circle-play');
+        play.classList.add('fa-circle-pause');        
+    }
+    else{
+        audio.pause();
+         play.classList.remove('fa-circle-pause');
+        play.classList.add('fa-circle-play'); 
+    }
+});
+
+audio.addEventListener('timeupdate',() =>{
+    let progress = (audio.currentTime/audio.duration)*100;
+    progressbar.value = progress;
+       progressbar.style.background =`linear-gradient(to right, #00920c ${progress}% , #333 ${progress}%)`
+
+})
+progressbar.addEventListener('input',function() {
+    let value = this.value;
+    this.style.background =`linear-gradient(to right, #00920c ${value}% , #333 ${value}%)`;
+    audio.currentTime = (progressbar.value*audio.duration)/100;
+})
+
+let playmusic= Array.from(document.getElementsByClassName('playmusic'));
+playmusic.forEach((element) =>{
+element.addEventListener('click', (e) =>{
+    e.target.classList.remove('fa-circle-play');
+    e.target.classList.add('fa-circle-pause');
+})
+}) 
