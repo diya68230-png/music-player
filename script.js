f3ab019df12c10aa6a1646f42519c6eb194ec360
Diya Shah <diya68230@gmail.com>
2026-04-19 15:21:22 +0530

    Play previous song and play next song functionality added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let play =document.getElementById('play');
 let progressbar =document.getElementById('progressbar');
 let audio = new Audio('Audio/1.mp3');
+let currentSong=1;
 play.addEventListener('click',() => {
     if(audio.paused || audio.currentTime == 0){
         audio.play();
@@ -44,8 +45,37 @@ playmusic.forEach((element) =>{
     play.classList.add('fa-circle-pause');
 
     index = parseInt(e.target.id);
+    currentSong= index;
     audio.src = `Audio/${index}.mp3`;
     audio.currentTime = 0;
     audio.play();
 })
-}) 
+});
+
+playNextSong = () => {
+    let nextSong = (currentSong+1)%playmusic.length;
+    currentSong = nextSong == 0 ? 29 :nextSong;
+    audio.src = `Audio/${currentSong}.mp3`;
+    audio.currentTime=0;
+    audio.play();
+}
+playPrevSong = () =>{
+   let PrevSong = (currentSong-1);
+    currentSong = PrevSong == 0 ? 29 :PrevSong;
+    audio.src = `Audio/${currentSong}.mp3`;
+    audio.currentTime=0;
+    audio.play(); 
+}
+
+forward= document.getElementById('forward');
+backward= document.getElementById('backward');
+
+forward.addEventListener('click',() => {
+    playNextSong();
+})
+audio.addEventListener('ended', () =>{
+
+})
+backward.addEventListener('click',() => {
+    playPrevSong();
+})
